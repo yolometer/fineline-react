72835c1dc62e3241f81e0358a7917bb404811936
Aaron Hamilton <aaron@correspondwith.me>
2014-09-26 10:21:02 +0000

    Add basic hello world react view to make sure things are working correctly.

---
+++ public/views/body.js
@@ -0,0 +1,7 @@
+var Hello = React.createClass({
+  render: function() {
+    return React.DOM.div({}, 'Hello ' + this.props.name);
+  }
+});
+
+React.renderComponent(Hello({name: 'World'}), document.body);
