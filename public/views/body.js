43187e103c430e85736aa11e162145c45b879a0d
Lance Stacenko <lancestacenko@me.com>
2014-09-29 00:09:37 -0400

    Pair coding sesh with aaron

--- public/views/body.js
+++ public/views/body.js
@@ -1,7 +1,18 @@
-var Hello = React.createClass({
-  render: function() {
-    return React.DOM.div({}, 'Hello ' + this.props.name);
-  }
+var hello = React.createClass({
+	  render: function() {
+  	  return React.DOM.svg({
+    		xmlns: "http://www.w3.org/2000/svg",
+    		width: window.innerWidth,
+	    	height: window.innerHeight,
+  	  	version: "1.1"
+    	}, null, [React.DOM.rect({x: 0, y: 0, width: window.innerWidth, height: 100, fill: "red"})]);
+  	}
 });
 
-React.renderComponent(Hello({name: 'World'}), document.body);
+
+
+React.renderComponent(hello(), document.body);
+
+window.onresize = function (){
+	React.renderComponent(hello(), document.body);
+}
