e3896a64ae3748a1b44db907ada19f9795d8a62b
Aaron Hamilton <aaron@correspondwith.me>
2014-10-05 04:17:17 +0000

    Add watch task to gulpfile called gulp dev.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat');
 
+gulp.task('dev', function () {
+  gulp.watch(['./app/views/*.js', './app/providers/*.js'], ['scripts']);
+});
+
 gulp.task('scripts', function() {
   gulp.src(['./app/views/*.js', './app/providers/*.js'])
     .pipe(concat('app.js'))
