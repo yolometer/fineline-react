999fcf1b69d7705f0e975e3226459597fb6039d4
Aaron Hamilton <aaron@correspondwith.me>
2014-10-05 08:04:26 +0000

    Add two gulp codepaths, one which inlines the javascript in index.html, and one which simply copies index.html from app/index.html.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var inlinesource = require('gulp-inline-source');
+var rename  = require('gulp-rename');
+
+
 
 var jsfiles = ['./app/bower/pleasejs/dist/Please.js', './app/bower/react/react.js', './app/views/*.js', './app/providers/*.js'];
+var indexhtml = ['./app/index.html'];
 
 gulp.task('dev', function () {
   gulp.watch(jsfiles, ['scripts']);
@@ -12,13 +17,28 @@ gulp.task('prod', function () {
   gulp.watch(jsfiles, ['compress']);
 });
 
-gulp.task('scripts', function() {
+gulp.task('dev_html', function () {
+  gulp.src(indexhtml)
+    .pipe(rename(function(path) {
+      path.dirname = "./public"
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('prod_html', function () {
+  gulp.src(indexhtml)
+    .pipe(inlinesource('./public'))
+    .pipe(gulp.dest('./public'));
+});
+
+
+gulp.task('scripts', function () {
   gulp.src(jsfiles)
     .pipe(concat('app.js'))
     .pipe(gulp.dest('./public/js'));
 });
 
-gulp.task('compress', function() {
+gulp.task('compress', function () {
   gulp.src(jsfiles)
     .pipe(concat('app.js'))
     .pipe(uglify())
